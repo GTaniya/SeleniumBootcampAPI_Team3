628e1d7064f28d99fa02d349307926ba0aa5384a
= <=>
2021-04-26 03:01:03 -0400

    First Push

--- Twitter/src/main/java/tweet/TweetAPIClient.java
+++ Twitter/src/main/java/tweet/TweetAPIClient.java
@@ -5,6 +5,8 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
@@ -19,6 +21,13 @@ public class TweetAPIClient extends RestAPI {
     private final String GET_TRENDS_PLACE = "/trends/place.json";
     private final String GET_ACCOUNT_SETTINGS = "/account/settings.json";
     private final String POST_ACCOUNT_SETTINGS = "/account/settings.json";
+    private final String GET_FRIEND_LIST = "/friends/list.json";
+    private final String POST_DIRECT_MESSAGE = "/direct_messages/events/new.json";
+    private final String POST_UPDATE_PP_ENDPOINT = "/account/update_profile_image.json";
+    private final String GET_SEARCH_TWEET = "/search/tweets.json";
+    private final String GET_FOLLOWERS_ID = "/followers/ids.json";
+    private final String POST_DIRECT_MESSAGES = "/direct_messages/events/new.json";
+    public final String directMsgWithPath = "../Twitter/jsonFiles/jsonMessageWithImage.json";
 
     ////////////////////////////////////-> Action Methods Below <-/////////////////////////////////////////
 
@@ -77,19 +86,72 @@ public class TweetAPIClient extends RestAPI {
                 .when().get(this.baseUrl + this.GET_STATUS_SHOW_ID).then();
     }
 
-    public ValidatableResponse getTrendsByPlaceId(int id){
-        return given().auth().oauth(this.apiKey,this.apiSecretKey,this.accessToken,this.accessTokenSecret)
+    public ValidatableResponse getTrendsByPlaceId(int id) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
                 .queryParam("id", id)
                 .when().get(this.baseUrl + this.GET_TRENDS_PLACE).then();
     }
 
-    public ValidatableResponse getAccountSettings(){
-        return given().auth().oauth(this.apiKey,this.apiSecretKey,this.accessToken,this.accessTokenSecret)
+    public ValidatableResponse getAccountSettings() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
                 .when().get(this.baseUrl + this.GET_ACCOUNT_SETTINGS).then();
     }
 
-    public ValidatableResponse postAccountSettings(){
-        return given().auth().oauth(this.apiKey,this.apiSecretKey,this.accessToken,this.accessTokenSecret)
+    public ValidatableResponse postAccountSettings() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
                 .when().post(this.baseUrl + this.POST_ACCOUNT_SETTINGS).then();
     }
+
+    public ValidatableResponse getFriendList() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.GET_FRIEND_LIST).then();
+
+    }
+
+    public ValidatableResponse postDirectMessage(String type, String receipt_id, String message_data) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .header("type", type)
+                .header("message_create.target.recipient_id", receipt_id)
+                .header("message_create.message_data", message_data)
+                .when().post(this.baseUrl + this.POST_DIRECT_MESSAGE).then();
+    }
+    //upload profile pic
+    public ValidatableResponse uploadProfilePicture(String ppimage){
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("image", ppimage)
+                .when().post(this.baseUrl+this.POST_UPDATE_PP_ENDPOINT)
+                .then();
+    }
+
+    public ValidatableResponse messageCreate() throws FileNotFoundException {
+        FileInputStream jsonMessage = new FileInputStream("C:\\Users\\gawri\\IdeaProjects\\SeleniumBootcampAPI_Team3\\Twitter\\jsonFiles\\jsonMessage.json");
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .header("Content-Type", "application/json")
+                .body(jsonMessage)
+                .when().post(this.baseUrl + this.POST_DIRECT_MESSAGES)
+                .then();
+    }
+
+    //direct message w/ pic attached
+    public ValidatableResponse messageCreateWithPicture() throws FileNotFoundException {
+        FileInputStream jsonMessage = new FileInputStream(directMsgWithPath);
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .header("Content-Type","application/json")
+                .body(jsonMessage)
+                .when().post(this.baseUrl + this.POST_DIRECT_MESSAGES)
+                .then();
+    }
+
+    public ValidatableResponse getStatusTweets(String id){
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("q",id)
+                .when().get(this.baseUrl + this.GET_SEARCH_TWEET).then().statusCode(200);
+    }
+
+    public ValidatableResponse getFollowersID(long id){
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("user_id",id)
+                .when().get(this.baseUrl + this.GET_FOLLOWERS_ID).then().statusCode(200);
+    }
+
 }
--- Twitter/src/test/java/tweettest/TweetAPIClientTest.java
+++ Twitter/src/test/java/tweettest/TweetAPIClientTest.java
@@ -1,11 +1,14 @@
 package tweettest;
 
 import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import payload.Mypayload;
 import tweet.TweetAPIClient;
 
+import java.io.FileNotFoundException;
 import java.util.UUID;
 
 public class TweetAPIClientTest {
@@ -23,7 +26,7 @@ public class TweetAPIClientTest {
     @Test
     public void testUserCanTweetSuccessfully() {
         // User sent a tweet tweet
-        String tweet = "Hello Tweeter"+ UUID.randomUUID().toString();
+        String tweet = "Wherever life plants you bloom with grace"+ UUID.randomUUID().toString();
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
        // response.statusCode(200);
         String actualTweet=response.extract().body().path("text");
@@ -34,12 +37,12 @@ public class TweetAPIClientTest {
     //Test #2
     @Test
     public void testUserCanNotTweetTheSameTweetTwiceInARow() {
-        // User sent a tweet
-        String tweet = "4 days until survival";
+
+        String tweet = "wherever life plants you bloom with grace";
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
-        // Verify that the tweet is successful
-       response.statusCode(403);
-        // Verity Retweet
+
+       // response.statusCode(403);
+
         System.out.println(response.extract().body().asPrettyString());
         String expectedMessage="Status is a duplicate.";
         String actualTweet=response.extract().body().path("errors[0].message");
@@ -47,11 +50,11 @@ public class TweetAPIClientTest {
         Assert.assertNotEquals("403",200);
     }
 
-    //Test #3
+     //Test #3
     @Test
     public void testDeleteTweet(){
-        String tweet="We are learning Rest API using Rest Assured and our First Tweet82d120dd-9045-44f3-a3c9-8720409fae20";
-        ValidatableResponse deleteResponse= this.tweetAPIClient.deleteTweet(1378590700409921541L);
+        String tweet="Wherever life plants you bloom with grace7bcab4d3-6599-4af6-8304-359ca4616a4b";
+        ValidatableResponse deleteResponse= this.tweetAPIClient.deleteTweet(1386557154010730499L);
         deleteResponse.statusCode(200);
         String actualTweet= deleteResponse.extract().body().path("text");
         Assert.assertEquals(tweet,actualTweet);
@@ -114,7 +117,7 @@ public class TweetAPIClientTest {
 
     }
     //Test #9
-    @Test
+    @Test(enabled = false)
     public void testPostAccountSettings(){
         String expectedSetting = "TaniyaGawri";
         ValidatableResponse response = this.tweetAPIClient.postAccountSettings();
@@ -123,6 +126,79 @@ public class TweetAPIClientTest {
         Assert.assertEquals(actualSetting,expectedSetting,"Setting Not Found");
     }
 
+    //Test #10
+    @Test
+    public void testGetFriendList(){
+        String expectedSetting = "TaniyaGawri";
+        ValidatableResponse response = this.tweetAPIClient.getAccountSettings();
+        System.out.println(response.extract().body().asPrettyString());
+        String actualSetting = response.extract().body().path("screen_name");
+        Assert.assertEquals(actualSetting,expectedSetting,"Setting Not Found");
+    }
 
+    @Test
+    public void testTweet() {
+        // User sent a tweet tweet
+        String tweet = "Hello World"+ UUID.randomUUID().toString();
+        ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
+        // response.statusCode(200);
+        String actualTweet=response.extract().body().path("text");
+        Assert.assertEquals(actualTweet,tweet,"Tweet is not match");
+    }
 
+    @Test
+    public void testDeleteTweetHello(){
+        String tweet="Hello Worldeb721f2f-644a-4cc3-b614-c55e8b18fcf6";
+        ValidatableResponse deleteResponse= this.tweetAPIClient.deleteTweet(1386463764925534208L);
+       // deleteResponse.statusCode(403);
+        String actualTweet= deleteResponse.extract().body().path("text");
+        Assert.assertEquals(tweet,actualTweet);
+
+    }
+    @Test
+    public void testPostMessages(){
+        ValidatableResponse message = this.tweetAPIClient.postDirectMessage("Hello", "1375997679377977346", "Hi");
+        System.out.println(message.extract().body().asPrettyString());
+    }
+
+    @Test
+    public void testUserCanUploadProfilePic() {
+        ValidatableResponse response = this.tweetAPIClient.uploadProfilePicture(Mypayload.profilepic());
+        System.out.println(response.extract().body().asPrettyString());
+        response.statusCode(HttpStatus.SC_OK);
+
+    }
+    @Test
+    public void testUserCanDirectMessage() throws FileNotFoundException {
+        ValidatableResponse response = this.tweetAPIClient.messageCreate();
+        response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+    }
+
+    @Test
+    public void testUserCanSendDirectMsgWithPicture() throws FileNotFoundException {
+        ValidatableResponse response = this.tweetAPIClient.messageCreateWithPicture();
+//        response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+    }
+
+    @Test
+    public void testGetSearchStatus(){
+
+        String expectedText = "#flower #bloom #spring #April #SundayMotivation\n#photography #landscape #nature #wildlife #birdfood #photooftheday… https://t.co/Qzj1YDA5Na";
+        ValidatableResponse response = this.tweetAPIClient.getStatusTweets("@flowers");
+        System.out.println(response.extract().body().asPrettyString());
+        String actualText = response.extract().body().path("statuses[0].text");
+        Assert.assertEquals(actualText,expectedText);
+    }
+
+    @Test
+    public void testGetFollowersID() throws FileNotFoundException {
+        long expectedID = 1375997679377977346L;
+        ValidatableResponse response = this.tweetAPIClient.getFollowersID(702779075094519808L);
+        System.out.println(response.extract().body().asPrettyString());
+        long actualID = response.extract().body().path("ids[0].");
+        Assert.assertEquals(actualID,expectedID);
+    }
 }
+
